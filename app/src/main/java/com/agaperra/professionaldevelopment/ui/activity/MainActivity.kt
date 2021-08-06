7dd0c1e9707c337e48a0b6727290b5241b7d7126
agaperra <agaperra@yandex.ru>
2021-08-06 16:49:17 +0300

    добавлен picasso, символика яндекса по ссылке из интернета

--- app/src/main/java/com/agaperra/professionaldevelopment/ui/activity/MainActivity.kt
+++ app/src/main/java/com/agaperra/professionaldevelopment/ui/activity/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.agaperra.professionaldevelopment.R
 import com.agaperra.professionaldevelopment.data.state.AppState
 import com.agaperra.professionaldevelopment.databinding.ActivityMainBinding
 import com.agaperra.professionaldevelopment.ui.adapter.MainAdapter
 import com.agaperra.professionaldevelopment.ui.base.BaseActivity
 import com.agaperra.professionaldevelopment.utils.Extensions.hide
 import com.agaperra.professionaldevelopment.utils.Extensions.show
+import com.squareup.picasso.Picasso
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -26,7 +28,9 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
         val mLink = binding.yandex
         mLink.movementMethod = LinkMovementMethod.getInstance()
-
+        Picasso.with(applicationContext).load("https://octavian48.ru/upload/iblock/df4/df4f7ad6adc88e74fadd6c26c3fe2ce1.png")
+            .placeholder(R.drawable.ic_baseline_image_not_supported_24).fit().centerInside()
+            .into(binding.ivYandex)
         initialize()
         setupListeners()
     }
